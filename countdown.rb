49c50233681d25e22a8d6358a602c9748e0383a3
Maryann Ruguru Njuguna <maryannruguru20@gmail.com>
2023-02-12 01:57:14 +0300

    completed aasignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(number)
+    until (number < 1)
+        puts "#{number} SECOND(S)!"
+        number -=1
+    end
+    return "HAPPY NEW YEAR!" 
+end
+
+def countdown_with_sleep(number)
+    until (number < 1)
+        puts "#{number} SECOND(S)!"
+        number -=1
+        sleep 5
+    end
+    return "HAPPY NEW YEAR!" 
 end
